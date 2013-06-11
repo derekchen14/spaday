d463be83755bd9aaeb4ffea958ae1ada433d0ccc
Derek Chen <derekchen14@gmail.com>
2013-06-11 09:49:23 -0700

    initial commit for spaday

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spaday::Application.config.secret_token = 'cad6143118e605449eb445e05c5a0cd2e213390ab8c699beb4dc4bc992c6269e20303fc66ed3b0b294698a26fde03f358658aecaa28aa4fd224e7338bcf2f323'
