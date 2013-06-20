dab1e908b8260f4143b3f782a881886e3158fe46
Derek Chen <derekchen14@gmail.com>
2013-06-19 22:17:30 -0700

    Add ability to add people

--- app/assets/javascripts/views/entries/entries_index.js
+++ app/assets/javascripts/views/entries/entries_index.js
@@ -2,10 +2,20 @@ var EntriesIndex = Backbone.View.extend({
   template: JST['entries/index'],
   initialize: function(){
     this.collection.on('reset', this.render, this);
+    this.collection.on('add', this.render, this);
+  },
+
+  events: {
+  	'submit #new_entry': 'addPeople'
   },
   render: function(){
     var text = {entries: this.collection} 
     this.$el.html(this.template(text));
     return this
+  },
+  addPeople: function (e) {
+  	e.preventDefault();
+  	this.collection.create({name: $('#new_entry_name').val()});
   }
+
 });
