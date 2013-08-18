b9546c4c5440742df2f28502fb4f56d14dc84186
Derek Chen <derekchen14@gmail.com>
2013-08-18 12:59:57 -0700

    Remove broken HandlebarAssets reference in backbone initializer

--- config/initializers/backbone.rb
+++ config/initializers/backbone.rb
@@ -1,5 +1,2 @@
 #To make sure backbone recieves the right JSON format
 ActiveRecord::Base.include_root_in_json = false
-
-#To use the JST namespace when referring to Handlebars
-HandlebarsAssets::Config.template_namespace = 'JST'
